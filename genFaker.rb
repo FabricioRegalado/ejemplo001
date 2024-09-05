8406bba5811e0bc7d6940920329b930893a95e3d
FabricioRegalado <l20290859@cdguzman.tecnm.mx>
2024-09-05 23:38:39 +0000

    agregamos los poderes de las gemas

--- genFaker.rb
+++ genFaker.rb
@@ -4,21 +4,21 @@ singers = []
 
 100.times do
   singer = {
-    name: Faker::Music.band,           # Genera un nombre de cantante
-    address: Faker::Address.full_address,  # Genera una dirección completa
-    email: Faker::Internet.email          # Genera un correo electrónico
+    nombre: Faker::Music.band,           # Genera un nombre de cantante
+    domicilio: Faker::Address.full_address,  # Genera una dirección completa
+    correo: Faker::Internet.email          # Genera un correo electrónico
   }
   singers << singer
 end
 
 singers.each_with_index do |singer, index|
-  puts "#{index + 1}. Nombre: #{singer[:name]}, Domicilio: #{singer[:address]}, Correo: #{singer[:email]}"
+  puts "#{index + 1}. Nombre: #{singer[:nombre]}, Domicilio: #{singer[:domicilio]}, Correo: #{singer[:correo]}"
 end
 
 #poder guardar lo generado en un archivo 
-File.open("listaGeneradaÑ.txt", "w") do |file|
+File.open("listaGenerada.txt", "w") do |file|
     singers.each_with_index do |singer, index|
-      file.puts "#{index + 1}. Nombre: #{singer[:name]}, Domicilio: #{singer[:address]}, Correo: #{singer[:email]}"
+      file.puts "#{index + 1}. Nombre: #{singer[:nombre]}, Domicilio: #{singer[:domicilio]}, Correo: #{singer[:correo]}"
     end
   end
   
